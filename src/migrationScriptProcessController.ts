710d19fb6008e2aeff89d835df21f9efc69da98c
Tim Vahlbrock <tim@vahlbrock.de>
2022-10-08 17:29:52 +0300

    Improve message interpretation in scriptprocesscontroller

--- src/migrationScriptProcessController.ts
+++ src/migrationScriptProcessController.ts
@@ -1,16 +1,22 @@
-import { ChildProcess, fork } from "child_process";
+import { ChildProcess, fork, Serializable } from "child_process";
 import { inject, injectable } from "inversify";
 import { join } from "path";
 import { TYPES, VscWindow, VSC_TYPES } from "./di/types";
+import { MigrationOutputChannel } from "./migration/migrationOutputChannel";
 import { MigrationStdOutChannel } from "./migration/migrationStdOutChannel";
+
+type SuccessListener = (result: any) => void;
+type ErrorListener = (error: any) => void;
 
 @injectable()
 export class MigrationScriptProcessController {
     private invocationCounter = 0;
     private child = this.createChild();
+    private invocationListeners: Record<number, [SuccessListener, ErrorListener]> = {};
 
     public constructor(
         @inject(TYPES.MigrationStdOutChannel) private readonly migrationStdOutChannel: MigrationStdOutChannel,
+        @inject(TYPES.MigrationOutputChannel) private readonly migrationOutputChannel: MigrationOutputChannel,
         @inject(VSC_TYPES.VscWindow) private readonly window: VscWindow
     ) { }
 
@@ -41,27 +47,35 @@ export class MigrationScriptProcessController {
                 });
             this.child = this.createChild();
         });
+        child.on("message", message => this.processMessage(message));
         return child;
+    }
+
+    private processMessage(message: Serializable): void {
+        try {
+            const parsedMessage = JSON.parse(message.toString());
+
+            const invocationId = parsedMessage.invocationId;
+            const handlers = this.invocationListeners[invocationId];
+
+            if (!handlers) {
+                return;
+            }
+
+            if (parsedMessage.error) {
+                handlers[1](parsedMessage.error);
+            } else {
+                handlers[0](parsedMessage.result);
+            }
+        } catch (error) {
+            this.migrationOutputChannel.append("Failed to parse message: " + message);
+        }
     }
 
     public send(target: string, methodName: string, ...args: any[]): Promise<any> {
         const invocationId = this.invocationCounter++;
         return new Promise<any>((res, rej) => {
-            const listener = (message: any): void => {
-                try {
-                    message = JSON.parse(message);
-                    if (message.invocationId === invocationId) {
-                        this.child.off("message", listener);
-
-                        if (message.error) {
-                            rej(message.error);
-                        } else {
-                            res(message.result);
-                        }
-                    }
-                } catch (ignore) { }
-            };
-            this.child.on("message", listener);
+            this.registerInvocationListener(invocationId, res, rej);
             try {
                 this.child.send({
                     invocationId: invocationId,
@@ -73,5 +87,13 @@ export class MigrationScriptProcessController {
                 rej(error);
             }
         });
+    }
+
+    private registerInvocationListener(
+        invocationId: number,
+        onSuccess: SuccessListener,
+        onError: ErrorListener
+    ): void {
+        this.invocationListeners[invocationId] = [onSuccess, onError];
     }
 }
