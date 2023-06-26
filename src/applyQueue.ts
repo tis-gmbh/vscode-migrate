e8e4b94d8bf1e1526f507eb9f2bf98a14f3d9c68
Tim Vahlbrock <tim@vahlbrock.de>
2023-06-26 22:19:57 +0200

    remove unnecessary queue functions

--- src/applyQueue.ts
+++ src/applyQueue.ts
@@ -2,32 +2,7 @@ import { injectable } from "inversify";
 
 @injectable()
 export class ApplyQueue {
-    private readonly queue: string[] = [];
-    public lastExecution?: Thenable<void>;
     private _isLocked = false;
-
-    public push(value: string): void {
-        this.queue.push(value);
-    }
-
-    public remove(value: string): void {
-        const index = this.queue.indexOf(value);
-        if (index >= 0) {
-            this.queue.splice(index, 1);
-        }
-    }
-
-    public includes(value: string): boolean {
-        return this.queue.includes(value);
-    }
-
-    public isEmpty(): boolean {
-        return this.queue.length === 0;
-    }
-
-    public isPreviousExecutionRunning(): boolean {
-        return !!this.lastExecution;
-    }
 
     public async lockWhile(blocking: () => Promise<void>): Promise<void> {
         this.throwIfLocked();
